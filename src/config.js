d8dc2792c3f99e19054b1b779d5e842c4890ee0b
Joshua Hutchinson <jthutch94@gmail.com>
2018-05-28 16:47:22 -0400

    fixed npm buid

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
-
-export const CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
+export const API_BASE_URL=process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080';
