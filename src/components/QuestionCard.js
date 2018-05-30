997ba9328d4a4ca8b6514e020c1aace7d4ac233a
Joshua Hutchinson <jthutch94@gmail.com>
2018-05-30 13:51:55 -0400

    working on form

--- src/components/QuestionCard.js
+++ src/components/QuestionCard.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import './QuestionCard.css';
 import requiresLogin from './requires-login';
+import { reduxForm, Field } from 'redux-form';
 import {fetchQuestions, submitAnswer} from '../actions/questions';
 
 export class QuestionCard extends React.Component {
@@ -14,24 +15,31 @@ export class QuestionCard extends React.Component {
     }
 
     render() {
+        console.log(this.props.questions);
+
         return (
-            <form className="question" >
+            <form className="question" onSubmit={this.props.handleSubmit(value => this.onSubmit(value.answer))}>
                 <p className="dothraki">
-                    {this.props.questions && this.props.questions[0].question}</p>
-                <input type="text" name="answer" placeholder="Answer" />
+                    {this.props.questions && this.props.questions.length > 0? this.props.questions[0].question : ''}</p>
+                <Field component="input" type="text" name="answer" placeholder="Answer" />
                 <br />
+                <p className="feedback">{this.props.feedback}</p>
                 <button className="submit">Submit</button>
-                <button className="next">Next Question</button>
+                <button className="next" onClick={() => this.props.dispatch(fetchQuestions())}>Next Question</button>
             </form>
         )
     };
 }
 
 const mapStateToProps = state => {
+    const { currentUser } = state.auth;
     return {
         questions: state.questions.data,
         feedback: state.questions.feedback
     }
 }
 
-export default requiresLogin()(connect(mapStateToProps)(QuestionCard));
+export default requiresLogin()(connect(mapStateToProps)(reduxForm({
+    form: 'answerQuestion'
+})(QuestionCard)));
+
